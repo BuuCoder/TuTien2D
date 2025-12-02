8ada8b6f89cf0ddf844b56551344b2653c24cd93
Phạm Quốc Khải <quockhai+mobcec@mobcec.com>
2025-12-02 17:33:48 +0700

    update port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const validator = require('./lib/validator');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 4004;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
