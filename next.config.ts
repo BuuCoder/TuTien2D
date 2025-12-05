8149ffbf6340987c3605d8d1384e59d255c0773f
Phạm Quốc Khải <quockhai+mobcec@mobcec.com>
2025-12-05 17:58:19 +0700

    webpack

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Add empty turbopack config to silence warning
-  turbopack: {},
 };
 
 export default nextConfig;
