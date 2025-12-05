9d0008e901ae7fbf3e17b45cac3edf20327b1672
Phạm Quốc Khải <quockhai+mobcec@mobcec.com>
2025-12-05 18:03:40 +0700

    Revert "webpack"
    This reverts commit 8149ffbf6340987c3605d8d1384e59d255c0773f.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Add empty turbopack config to silence warning
+  turbopack: {},
 };
 
 export default nextConfig;
